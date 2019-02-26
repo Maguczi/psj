f725d61d7cd9cd6b9f55dbfe1c38887d78e9c8ad
Greg <g.kawalec@live.com>
2019-02-26 21:50:40 +0100

    start

---
+++ src/Container/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+const header = () => {
+  return (
+    <header>
+      LOGO
+      <br />
+      <Navigation />
+    </header>
+  )
+}
+
+export default header
---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+const routes = [
+  {
+    path: '/news',
+    component: 'News'
+  },
+  {
+    path: '/magazine',
+    component: 'Magazine'
+  },
+  {
+    path: '/contact',
+    component: 'Contact'
+  }
+]
+
+export default routes
